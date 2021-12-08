27f89f35ecbc43d520fb1723af0698b1a4e7969c
jackiem2 <jacqueline.merlan@gmail.com>
2021-12-07 23:12:30 -0600

    added playerName variable

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName 
+window.alert(playerName);
+
+function fight() {
+window.alert("The fight has begun!");
+}
+// fight();
