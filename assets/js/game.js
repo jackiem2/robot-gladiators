f395a2f4ea338f2a1e56f8adeaa9cc2b8f887f74
jackiem2 <jacqueline.merlan@gmail.com>
2021-12-13 19:43:51 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,14 +28,15 @@ for(var i = 0; i < enemyNames.length; i++) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -54,7 +55,8 @@ for(var i = 0; i < enemyNames.length; i++) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -85,7 +87,8 @@ for(var i = 0; i < enemyNames.length; i++){
       var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+
+      enemyHealth = randomNumber(40, 60);
 
     //use debugger to pause script from running and check what's going on at the moment in the code
     // debugger;
@@ -162,6 +165,14 @@ switch (shopOptionPrompt) {
     break;
   } 
 };
+
+// function to generate a random numeric value
+var randomNumber = function () {
+  var value = Math.floor(Math.random() * 21) + 40;
+
+  return value;
+};
+
 // ask player if they'd like to play again
 var playAgainConfirm = window.confirm("Would you like to play again?");
 
